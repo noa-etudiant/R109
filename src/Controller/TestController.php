7cf8642e085133076ef42e660b9a7f58bf4ac2a0
Noa <noa.tavernier@etu.univ-st-etienne.fr>
2025-01-08 10:54:19 +0100

    msj0801

---
+++ src/Controller/TestController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test', name: 'app_test')]
+    public function index(): Response
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+    #[Route('/test/home', name: 'app_home')]
+    public function home() : response
+    {return $this->render('test/home.html.twig') ;}
+}
+
+
